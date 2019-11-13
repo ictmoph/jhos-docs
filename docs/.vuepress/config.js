0c50b00a5a200a739ee5a4cc1fa8a64c02a4b153
Leenawat <leenawat.pa@gmail.com>
2019-11-13 21:47:13 +0700

    add jhos docs system

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,37 @@
 module.exports = {
-    title: 'Scotch VuePress',
-    description: "A demo documentation using VuePress",
-    themeConfig:{
-        nav: [
-            { text: 'COUNTER', link: '/counter/' },
-            { text: 'GUIDE', link: '/guide/' },
-        ],
-        sidebar: [
-            {
-              title: 'Counter',
-              collapsable: false,
-              children: [
-                '/counter/counter-app'
-              ]
-            },
-            {
-              title: 'API Guide',
-              collapsable: false,
-              children: [
-                  '/guide/guide',
-                  '/guide/api'
-              ]
-            }
-          ]
-    }
+  title: 'JHOS Manual',
+  description: "A demo documentation using VuePress",
+  themeConfig: {
+    nav: [
+      { text: 'คู่มือผู้ใช้', link: '/user-manual/' },
+      { text: 'ผู้ดูแลระบบ', link: '/config/' },
+      { text: 'การติดตั้ง', link: '/install/' }
+    ],
+    sidebar: [
+      {
+        title: 'คู่มือผู้ใช้',
+        collapsable: false,
+        children: [
+          '/user-manual/login',
+          '/user-manual/patient-new'
+        ]
+      },
+      {
+        title: 'คู่มือผู้ดูแลระบบ',
+        collapsable: false,
+        children: [
+          '/config/system'
+        ]
+      },
+      {
+        title: 'การติดตั้ง',
+        collapsable: false,
+        children: [
+          '/install/mysql',
+          '/install/jdk',
+          '/install/tomcat'
+        ]
+      }
+    ]
+  }
 }
