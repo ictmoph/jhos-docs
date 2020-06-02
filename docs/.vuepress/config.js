f1aef406eacdc63d33ae1f7db7fd7a8b92fd1a1c
plumekamol <plumekamol@gmail.com>
2020-06-02 14:52:32 +0700

    เพิ่มเมนู 215

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,6 +49,7 @@ module.exports = {
           '/user-manual/212-ipd-move-building',
           '/user-manual/213-ipd-food-order',
           '/user-manual/214-ipd-confirm-food-order',
+          '/user-manual/215-gohome',
           '/user-manual/300-finance',
           '/user-manual/301-discharge',
           '/user-manual/302-cancel-receipt',
