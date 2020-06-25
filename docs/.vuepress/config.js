9d241d4bbb4df6857b07aa4043e78a5505e47079
plumekamol <plumekamol@gmail.com>
2020-06-25 12:27:25 +0700

    เพิ่มเมนู 503

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,6 +62,7 @@ module.exports = {
           '/user-manual/403-STI',
           '/user-manual/404-set-code-cost',
           '/user-manual/501-drug-utilization',
+          '/user-manual/503-drug-allergy',
       
         ]
       },
