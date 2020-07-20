bec3e3309712a86874f5222cfa74eb2b6a6ea678
plumekamol <plumekamol@gmail.com>
2020-07-20 15:05:54 +0700

    เพิ่มเมนู 708

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -79,6 +79,7 @@ module.exports = {
           '/admin-manual/705-admin-add-user',
           '/admin-manual/706-admin-add-group-user',
           '/admin-manual/707-admin-add-user-menu',
+          '/admin-manual/708-admin-add-prescription-clinic',
 
         ]
       },
